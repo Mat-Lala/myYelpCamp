300a5289e507639267900ba615606a436e4d4f75
Mat-Lala <matthieu.lassartesses@gmail.com>
2022-10-18 17:27:31 -0700

    commit october 18th: error handling

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError')
+const methodOverride = require('method-override')
 const path = require('path');
 const Campground = require('./models/campground');
+const { error } = require('console');
 
 const app = express();
 
 //To parse form data in POST request body:
 app.use(express.urlencoded({ extended: true }))
+// To 'fake' put/patch/delete requests:
+app.use(methodOverride('_method'))
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -20,23 +28,53 @@ app.get('/campgrounds', async(req,res)  => {
   res.render('campgrounds/index', {campgrounds})
 })
 
-app.get('/campgrounds/:id', async(req,res)  => {
+app.get('/campgrounds/:id', catchAsync(async(req,res)  => {
     let {id} = req.params
     const campground =  await Campground.findById(id)
     console.log(campground)
     res.render('campgrounds/show', {campground})
-  })
+  }))
 
 app.get("/new",  (req, res)=> {
     res.render('campgrounds/new')
 })  
 
-app.post('/campgrounds', async(req,res)  => {
-    console.log(req.body)
-    const newCampground = new Campground(req.body);
-    const campgrounds = await newCampground.save();
+app.post('/campgrounds', catchAsync( async(req,res)  => {
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
     res.redirect('/campgrounds')
-  })
+  }))
+
+app.get("/campgrounds/:id/edit",  async (req, res)=> {
+    const {id} = req.params
+    console.log(`yep this is a edit request for product ${id}`)
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/edit', { campground })
+})
+
+app.put("/campgrounds/:id",  catchAsync(async (req, res)=> {
+    const { id } = req.params
+    console.log(`let s go update campground ${id}`)
+    const campground = await Campground.findByIdAndUpdate(id, req.body.campground)
+    res.redirect('/campgrounds')
+}))
+
+app.delete("/campgrounds/:id",  async (req, res)=> {
+    const { id } = req.params
+    console.log(`let s go delete camp ${id}`)
+    const campground = await Campground.findByIdAndDelete(id)
+    res.redirect('/Campgrounds')
+})
+
+app.all('*', (req,res,next) => {
+  next(new ExpressError('Page Not Found',404))
+})
+
+app.use((err, req, res, next) => {
+  const {statusCode = 500 } = err;
+  if(!err.message) err.message = 'Oh no, something went wrong'
+  res.status(statusCode).render('error', {err})
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
--- seeds/index.js
+++ seeds/index.js
@@ -10,9 +10,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random()*20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/random/500×300/?camping',
+            description: 'Lorem blablalblalblalbalblsallablalbaalalfld laaldfldlfldlfldflf',
+            price
         })
         await camp.save();
     }
